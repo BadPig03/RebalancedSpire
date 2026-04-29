8c342e9ab3586e64b0752079a196ab381416091e
ty <2305597132@qq.com>
2026-04-30 00:26:01 +0800

    v0.0.10_beta预更新。

---
+++ scr/Core/Potions/BoneTeaPotion.cs
@@ -0,0 +1,38 @@
+﻿namespace RebalancedSpire.scr.Core.Potions;
+
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using Godot;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.PotionPools;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+
+[Pool(typeof(SharedPotionPool))]
+public sealed class BoneTeaPotion : CustomPotionModel
+{
+    public override PotionRarity Rarity => PotionRarity.Event;
+
+    public override PotionUsage Usage => PotionUsage.CombatOnly;
+
+    public override TargetType TargetType => TargetType.Self;
+
+    public override Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
+    {
+        NCombatRoom.Instance?.PlaySplashVfx(Owner.Creature, new Color("cd683d"));
+        foreach (CardModel card in PileType.Draw.GetPile(Owner).Cards)
+        {
+            if (!card.IsUpgradable)
+            {
+                continue;
+            }
+
+            CardCmd.Upgrade(card);
+        }
+        return Task.CompletedTask;
+    }
+}
